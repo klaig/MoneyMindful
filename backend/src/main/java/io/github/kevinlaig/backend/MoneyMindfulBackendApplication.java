ec5a028521cfbe6296d231dcefbb9e36412c0fa7
Kevin Laig <laig.kevin@gmail.com>
2024-03-25 19:10:52 +0200

    Initial commit of backend Spring Boot project

---
+++ backend/src/main/java/io/github/kevinlaig/backend/MoneyMindfulBackendApplication.java
@@ -0,0 +1,13 @@
+package io.github.kevinlaig.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyMindfulBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoneyMindfulBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/io/github/kevinlaig/backend/MoneyMindfulBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.kevinlaig.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoneyMindfulBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
