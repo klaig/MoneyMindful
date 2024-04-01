82764ea0e31bf9547128123a27d7b487cf524a2a
Kevin Laig <laig.kevin@gmail.com>
2024-04-01 15:37:37 +0300

    All tests should be fixed now to work properly in Github Actions

--- backend/src/test/java/io/github/kevinlaig/backend/MoneyMindfulBackendApplicationTests.java
+++ backend/src/test/java/io/github/kevinlaig/backend/MoneyMindfulBackendApplicationTests.java
@@ -2,8 +2,10 @@ package io.github.kevinlaig.backend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class MoneyMindfulBackendApplicationTests {
 
 	@Test
