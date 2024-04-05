cd0c63ae1bad24134d3979dd83d2915c5bae5c8f
Kevin Laig <laig.kevin@gmail.com>
2024-04-05 09:50:58 +0300

    Edited tests to be relevant to my current app structure

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,8 +1,17 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders Layout and main routes', () => {
+  render(
+      <Router>
+        <App />
+      </Router>
+  );
+  // Check for a specific element that is always rendered by the Layout component
+  const overviewElement = screen.getByText(/overview/i);
+  expect(overviewElement).toBeInTheDocument();
+
 });
